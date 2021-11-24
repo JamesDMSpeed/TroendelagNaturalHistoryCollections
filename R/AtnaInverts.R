0471b2637c3b37ebfe096d745099ba0b0060931d
JamesDMSpeed <james.speed@ntnu.no>
2021-11-24 21:43:57 +0100

    Updating Atna analyses with data from Annie. Improvements to forest plot

--- R/AtnaInverts.R
+++ R/AtnaInverts.R
@@ -1,7 +1,8 @@
 #Atna invertebrates
 
-atnadat<-read.csv("Data/Inverts/Richness_Ephemeroptera_Plecoptera_Updated.csv",header=T,sep=";")
-olddat<-read.csv("Data/Inverts/Richness_Ephemeroptera.csv",header=T,sep=";")
+#atnadat<-read.csv("Data/Inverts/Richness_Ephemeroptera_Plecoptera_Updated.csv",header=T,sep=";")
+atnadat<-read.csv("Data/Inverts/Richness_Ephemeroptera_Plecoptera_Allgenera.csv",header=T,sep=";")
+#olddat<-read.csv("Data/Inverts/Richness_Ephemeroptera.csv",header=T,sep=";")
 #Climate data
 climdata<-read.csv("Data/ClimateData_forAnalyses.csv",header=T)
 
@@ -21,19 +22,37 @@ atnadat$Elevation[atnadat$ï..Site=='Solbakken']<-380
 stationdf<-data.frame(with(atnadat,tapply(Total.Species.Richness,list(Year,Elevation),max,na.rm=T)))
 stationdf$Year<-as.numeric(rownames(stationdf))
 
-with(stationdf,plot(Year,X380,type='b',ylim=c(0,10),ylab="Species richness",las=1))
+with(stationdf,plot(Year,X380,type='b',ylim=c(0,15),ylab="Species richness",las=1))
 with(stationdf,lines(Year,X710,type='b',col='red'))
 with(stationdf,lines(Year,X1060,type='b',col='blue'))
 with(stationdf,lines(Year,X1120,type='b',col='green'))
+abline(v=2002.5)
 legend('topl',lty=1,col=c(1,'red','blue','green'),c('380m','710m','1060m',"1120"),ncol=3)
 
 with(stationdf,cor.test(Year,X380))
 with(stationdf,cor.test(Year,X710))
 with(stationdf,cor.test(Year,X1060))
 
+stationdf$timeperiod<-stationdf$Year
+stationdf$timeperiod[stationdf$Year<=2002]<-'old'
+stationdf$timeperiod[stationdf$Year>2002]<-'new'
+
+t.test(stationdf$X380~stationdf$timeperiod)
+t.test(stationdf$X710~stationdf$timeperiod)
+t.test(stationdf$X1060~stationdf$timeperiod)
+t.test(stationdf$X1120~stationdf$timeperiod)
+
+lmST<-with(stationdf,lm(X710~Year+timeperiod))
+summary(lmST)
+
 
 #Merge with climate data
 stationdfclim<-merge(stationdf,climdata,all.x=T,all.y=F,by="Year")
+
+lmSTemp<-with(stationdfclim[stationdfclim$timeperiod=='old',],lm(X1060~Annual))
+lmSTemp<-with(stationdfclim,lm(X1120~Annual+timeperiod))
+summary(lmSTemp)
+
 
 #Dataframe with temporal correlations
 atnacorYeardf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
@@ -52,15 +71,27 @@ atnacorYeardf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
 atnacorYeardf
 
 #Dataframe with temporal regression correlations
+# atnaregYeardf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
+#                           b=c(with(stationdf,summary(lm(X380~Year))$coefficients[2,1]),
+#                               with(stationdf,summary(lm(X710~Year))$coefficients[2,1]),
+#                               with(stationdf,summary(lm(X1060~Year))$coefficients[2,1]),
+#                               with(stationdf,summary(lm(X1120~Year))$coefficients[2,1])),
+#                           se=c(with(stationdf,summary(lm(X380~Year))$coefficients[2,2]),
+#                               with(stationdf,summary(lm(X710~Year))$coefficients[2,2]),
+#                               with(stationdf,summary(lm(X1060~Year))$coefficients[2,2]),
+#                               with(stationdf,summary(lm(X1120~Year))$coefficients[2,2])))
+
 atnaregYeardf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
-                          b=c(with(stationdf,summary(lm(X380~Year))$coefficients[2,1]),
-                              with(stationdf,summary(lm(X710~Year))$coefficients[2,1]),
-                              with(stationdf,summary(lm(X1060~Year))$coefficients[2,1]),
-                              with(stationdf,summary(lm(X1120~Year))$coefficients[2,1])),
-                          se=c(with(stationdf,summary(lm(X380~Year))$coefficients[2,2]),
-                              with(stationdf,summary(lm(X710~Year))$coefficients[2,2]),
-                              with(stationdf,summary(lm(X1060~Year))$coefficients[2,2]),
-                              with(stationdf,summary(lm(X1120~Year))$coefficients[2,2])))
+                          b=c(with(stationdf,summary(lm(X380~Year+timeperiod))$coefficients[2,1]),
+                              with(stationdf,summary(lm(X710~Year+timeperiod))$coefficients[2,1]),
+                              with(stationdf,summary(lm(X1060~Year+timeperiod))$coefficients[2,1]),
+                              with(stationdf,summary(lm(X1120~Year+timeperiod))$coefficients[2,1])),
+                          se=c(with(stationdf,summary(lm(X380~Year+timeperiod))$coefficients[2,2]),
+                               with(stationdf,summary(lm(X710~Year+timeperiod))$coefficients[2,2]),
+                               with(stationdf,summary(lm(X1060~Year+timeperiod))$coefficients[2,2]),
+                               with(stationdf,summary(lm(X1120~Year+timeperiod))$coefficients[2,2])))
+
+forest(rma(escalc(yi=atnaregYeardf$b,sei=atnaregYeardf$se,measure="GEN")))
 atnaregYeardf
 
 #Dataframe with temporal regression correlations
@@ -73,6 +104,18 @@ atnaregYearScaledf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
                                with(stationdf,summary(lm(scale(X710)~Year))$coefficients[2,2]),
                                with(stationdf,summary(lm(scale(X1060)~Year))$coefficients[2,2]),
                                with(stationdf,summary(lm(scale(X1120)~Year))$coefficients[2,2])))
+
+atnaregYearScaledf<-data.frame(Elevation=c('380m','710m','1060m',"1120m"),
+                               b=c(with(stationdf,summary(lm(scale(X380)~Year+timeperiod))$coefficients[2,1]),
+                                   with(stationdf,summary(lm(scale(X710)~Year+timeperiod))$coefficients[2,1]),
+                                   with(stationdf,summary(lm(scale(X1060)~Year+timeperiod))$coefficients[2,1]),
+                                   with(stationdf,summary(lm(scale(X1120)~Year+timeperiod))$coefficients[2,1])),
+                               se=c(with(stationdf,summary(lm(scale(X380)~Year+timeperiod))$coefficients[2,2]),
+                                    with(stationdf,summary(lm(scale(X710)~Year+timeperiod))$coefficients[2,2]),
+                                    with(stationdf,summary(lm(scale(X1060)~Year+timeperiod))$coefficients[2,2]),
+                                    with(stationdf,summary(lm(scale(X1120)~Year+timeperiod))$coefficients[2,2])))
+
+
 atnaregYearScaledf
 
 
@@ -90,27 +133,51 @@ atnacorTempdf<-data.frame(Elevation=c('380m','710m','1060m','1120m'),
 atnacorTempdf
 
 #Dataframe with temperature regression correlations
+# atnaregAnnualdf<-data.frame(Elevation=c('380m','710m','1060m','1120m'),
+#                           b=c(with(stationdfclim,summary(lm(X380~Annual))$coefficients[2,1]),
+#                               with(stationdfclim,summary(lm(X710~Annual))$coefficients[2,1]),
+#                               with(stationdfclim,summary(lm(X1060~Annual))$coefficients[2,1]),
+#                               with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,1])),
+#                           se=c(with(stationdfclim,summary(lm(X380~Annual))$coefficients[2,2]),
+#                                with(stationdfclim,summary(lm(X710~Annual))$coefficients[2,2]),
+#                                with(stationdfclim,summary(lm(X1060~Annual))$coefficients[2,2]),
+#                                with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,2])))
+
 atnaregAnnualdf<-data.frame(Elevation=c('380m','710m','1060m','1120m'),
-                          b=c(with(stationdfclim,summary(lm(X380~Annual))$coefficients[2,1]),
-                              with(stationdfclim,summary(lm(X710~Annual))$coefficients[2,1]),
-                              with(stationdfclim,summary(lm(X1060~Annual))$coefficients[2,1]),
-                              with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,1])),
-                          se=c(with(stationdfclim,summary(lm(X380~Annual))$coefficients[2,2]),
-                               with(stationdfclim,summary(lm(X710~Annual))$coefficients[2,2]),
-                               with(stationdfclim,summary(lm(X1060~Annual))$coefficients[2,2]),
-                               with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,2])))
+                            b=c(with(stationdfclim,summary(lm(X380~Annual+timeperiod))$coefficients[2,1]),
+                                with(stationdfclim,summary(lm(X710~Annual+timeperiod))$coefficients[2,1]),
+                                with(stationdfclim,summary(lm(X1060~Annual+timeperiod))$coefficients[2,1]),
+                                with(stationdfclim,summary(lm(X1120~Annual+timeperiod))$coefficients[2,1])),
+                            se=c(with(stationdfclim,summary(lm(X380~Annual+timeperiod))$coefficients[2,2]),
+                                 with(stationdfclim,summary(lm(X710~Annual+timeperiod))$coefficients[2,2]),
+                                 with(stationdfclim,summary(lm(X1060~Annual+timeperiod))$coefficients[2,2]),
+                                 with(stationdfclim,summary(lm(X1120~Annual+timeperiod))$coefficients[2,2])))
+
 atnaregAnnualdf
 
+forest(rma(escalc(yi=atnaregAnnualdf$b,sei=atnaregAnnualdf$se,measure="GEN")))
+
+
 #Dataframe with scaled temperature regression correlations
+# atnaregAnnualScaledf<-data.frame(Elevation=c('380m','710m','1060m','1120m'),
+#                             b=c(with(stationdfclim,summary(lm(scale(X380)~Annual))$coefficients[2,1]),
+#                                 with(stationdfclim,summary(lm(scale(X710)~Annual))$coefficients[2,1]),
+#                                 with(stationdfclim,summary(lm(scale(X1060)~Annual))$coefficients[2,1]),
+#                                 with(stationdfclim,summary(lm(scale(X1120)~Annual))$coefficients[2,1])),
+#                             se=c(with(stationdfclim,summary(lm(scale(X380)~Annual))$coefficients[2,2]),
+#                                  with(stationdfclim,summary(lm(scale(X710)~Annual))$coefficients[2,2]),
+#                                  with(stationdfclim,summary(lm(scale(X1060)~Annual))$coefficients[2,2]),
+#                                  with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,2])))
 atnaregAnnualScaledf<-data.frame(Elevation=c('380m','710m','1060m','1120m'),
-                            b=c(with(stationdfclim,summary(lm(scale(X380)~Annual))$coefficients[2,1]),
-                                with(stationdfclim,summary(lm(scale(X710)~Annual))$coefficients[2,1]),
-                                with(stationdfclim,summary(lm(scale(X1060)~Annual))$coefficients[2,1]),
-                                with(stationdfclim,summary(lm(scale(X1120)~Annual))$coefficients[2,1])),
-                            se=c(with(stationdfclim,summary(lm(scale(X380)~Annual))$coefficients[2,2]),
-                                 with(stationdfclim,summary(lm(scale(X710)~Annual))$coefficients[2,2]),
-                                 with(stationdfclim,summary(lm(scale(X1060)~Annual))$coefficients[2,2]),
-                                 with(stationdfclim,summary(lm(X1120~Annual))$coefficients[2,2])))
+                                 b=c(with(stationdfclim,summary(lm(scale(X380)~Annual+timeperiod))$coefficients[2,1]),
+                                     with(stationdfclim,summary(lm(scale(X710)~Annual+timeperiod))$coefficients[2,1]),
+                                     with(stationdfclim,summary(lm(scale(X1060)~Annual+timeperiod))$coefficients[2,1]),
+                                     with(stationdfclim,summary(lm(scale(X1120)~Annual+timeperiod))$coefficients[2,1])),
+                                 se=c(with(stationdfclim,summary(lm(scale(X380)~Annual+timeperiod))$coefficients[2,2]),
+                                      with(stationdfclim,summary(lm(scale(X710)~Annual+timeperiod))$coefficients[2,2]),
+                                      with(stationdfclim,summary(lm(scale(X1060)~Annual+timeperiod))$coefficients[2,2]),
+                                      with(stationdfclim,summary(lm(X1120~Annual+timeperiod))$coefficients[2,2])))
+
 atnaregAnnualScaledf
 
 
